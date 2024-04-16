7c5edb68a99708b77ead9492a85739c29384d2a3
Jose Serna <josesernag@hotmail.com>
2024-04-16 16:29:50 -0500

    Data is retrieved now from JSON file

--- src/graphql/resolvers/character.ts
+++ src/graphql/resolvers/character.ts
@@ -1,23 +1,26 @@
 import {IResolvers} from "@graphql-tools/utils";
+import data from '../../data/data.json'
 
 export const characterResolver: IResolvers = {
     Query: {
         hello() {
             return "world"
         },
+        getCharacter(root: void, args: any) {
+            const [found] = data.characters.filter(ch => ch._id === args._id)
+            return found
+        },
         getCharacters() {
-            return [
-                {
-                    id: 1,
-                    name: 'Link',
-                    race: 'HYLIAN'
-                },
-                {
-                    id: 2,
-                    name: 'Zelda',
-                    race: 'HYLIAN'
-                }
-            ]
+            return data.characters
+        }
+    },
+    Character: {
+        games(parent: any) {
+            const gameList: Array<any> = []
+            parent.games.map((gameId: string) =>
+                gameList.push(...data.games.filter(game => game._id === gameId))
+            )
+            return gameList
         }
     }
 }
